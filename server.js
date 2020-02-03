9c8243ed865aca722263a48f3ccb6f472d53ed88
Christian Benseler <cbenseler@gmail.com>
2020-02-03 11:49:24 -0300

    feat: task list resolver and get task by id

--- server.js
+++ server.js
@@ -3,19 +3,47 @@ const { ApolloServer, gql } = require('apollo-server-express')
 const cors = require('cors')
 const dotEnv = require('dotenv')
 
+const { tasks, users } = require('./constants')
+
 dotEnv.config()
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 const typeDefs = gql`
     type Query {
-        greetings: String
+        greetings: [String!]
+        tasks: [Task!]
+        task(id: ID!): Task
+    }
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        tasks: [Task!]
+    }
+
+    type Task {
+        id: ID!
+        name: String!
+        completed: Boolean!,
+        user: User!
     }
 `
 
-const resolvers = {}
+const resolvers = {
+    Query: {
+        greetings: () => ['Hello', 'World'],
+        tasks: () => tasks,
+        task: (_, args) => tasks.find(task => task.id === args.id) 
+    },
+    Task: {
+        user: ({ userId}) => users.find(user => user.id === userId)
+    }
+}
 
 const apolloServer = new ApolloServer({
     typeDefs,
