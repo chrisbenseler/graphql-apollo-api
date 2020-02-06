7501f1ff90936b6baa9677dfce4025f72ca47709
Christian Benseler <cbenseler@gmail.com>
2020-02-06 08:41:57 -0300

    feat: sign up mutation

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcryptjs')
 const { users, tasks }  = require('../constants')
+const User = require('../database/models/user') 
 
 module.exports = {
     Query: {
@@ -7,5 +9,21 @@ module.exports = {
     },
     User: {
         tasks: ({ id }) => tasks.filter(task => task.userId === id)
+    },
+    Mutation: {
+        signUp: async (_, { input }) => {
+            try {
+                const user = await User.findOne({ email: input.email })
+                if(user)
+                    throw new Error('E-mail already in use')
+                const hashedPassword = await bcrypt.hash(input.password, 10)
+                const newUser = new User({ ...input, password: hashedPassword })
+                const result = await newUser.save()
+                return result
+            } catch(e) {
+                console.error(e)
+                throw e;
+            }
+        }
     }
 }
--- typeDefs/user.js
+++ typeDefs/user.js
@@ -6,9 +6,21 @@ module.exports = gql`
         user(id: ID!): User
     }
 
+    extend type Mutation {
+        signUp(input: signUpInput): User
+    }
+
+    input signUpInput {
+        name: String!
+        email: String!
+        password: String!
+    }
+
     type User {
         id: ID!
         name: String!
         email: String!
         tasks: [Task!]
+        createdAt: String!
+        updatedAt: String!
     }`
